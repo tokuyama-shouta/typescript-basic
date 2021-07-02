ebabf8bd584957ca15a6eaeaae728b3282a06405
tokuyama shouta <toku1031ts@gmail.com>
2021-07-02 21:17:37 +0900

    オブジェクト思考開発

--- src/oop/shogi.ts
+++ src/oop/shogi.ts
@@ -1,19 +1,58 @@
 type Suji = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 type Dan = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
-type Player = 'furst' | 'second'
+type Player = 'first' | 'second'
 
 // 駒の位置を表すクラス
 
 class Position {
   constructor(private suji: Suji, private dan: Dan) {}
+
+  //パラメーターに渡された位置と現在の位置を比較するメソッド
+  distanceFrom(position: Position, player: Player) {
+    if (player === 'first') {
+      return {
+        suji: Math.abs(position.suji - this.suji),
+        dan: Math.abs(Number(position.dan) - Number(this.dan)),
+      }
+    } else {
+      return {
+        suji: Math.abs(position.suji - this.suji),
+        dan: -Math.abs(Number(position.dan) - Number(this.dan)), //正と負を反転させる
+      }
+    }
+  }
 }
 new Position(1, '2')
 
 // コマを表すクラス
-class Piece {
-  position: Position
+abstract class Piece {
+  // pieceクラスとサブクラスでアクセスできる
+  protected position: Position
 
   constructor(private readonly player: Player, suji: Suji, dan: Dan) {
     this.position = new Position(suji, dan)
+  }
+  // メソッドの定義
+  // 駒の移動用のメソッド
+  moveTo(position: Position) {
+    this.position = position
+  }
+
+  // 移動できるかどうかを判定するメソッド
+  abstract canMoveTo(position: Position, player: Player): boolean
+}
+
+class Osho extends Piece {
+  // 王将のcanMoveToメソッドを具体的に実装する
+  canMoveTo(position: Position, player: Player): boolean {
+    const distance = this.position.distanceFrom(position, player)
+    return distance.suji < 2 && distance.dan < 2
+  }
+}
+
+class Game {
+  private pieces = Game.makePieces()
+  private static makePieces() {
+    return [new Osho('first', 5, '1'), new Osho('second', 5, '9')]
   }
 }
